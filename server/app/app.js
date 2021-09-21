707240a6cc62977513cbb0aa84064b6201c54995
gonudayo <kgw010119@gmail.com>
2021-09-21 07:25:09 +0000

    api

---
+++ server/app/app.js
@@ -0,0 +1,87 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const config = require('./src/config/key');
+const { User } = require('./src/models/User');
+const { auth } = require('./src/middleware/auth')
+
+
+mongoose
+.connect(config.mongoURI)
+.then(() => console.log("3 2 1 Lift Off ."))
+.catch((e) => console.log('MongoDB error: ', e));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.post('/api/users/register', (req, res) => {
+	const user = new User(req.body);
+	
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err});
+		return res.status(200).json({
+			success: true
+		});
+	});
+});
+
+app.post('/api/users/login', (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if(!user) {
+			return res.json({
+				loginSuccess: false,
+				message: "존재하지 않는 이메일 입니다."
+			});
+		}
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch) {
+				return res.json({
+					loginSuccess: false,
+					message: "비밀번호가 틀렸습니다."
+				});
+			}
+			user.generateToken((err, user) => {
+				if(err) return res.status(400).send(err);
+				res.cookie("x_auth", user.token)
+				.status(200)
+				.json({
+					loginSuccess: true, userId: user._id
+				});
+			});
+		});
+	});
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+	res.status(200).json({
+		_id: req.user._id,
+		isAdmin: req.user.role === 0 ? false : true,
+		isAuth: true,
+		email: req.user.email,
+		name: req.user.name
+	});
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+	User.findOneAndUpdate({ _id: req.user._id },{ token: "" }, (err, user) => {
+		if(err) return res.json({ success: false, err });
+		return res.status(200).send({ success: true });
+	});
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Engine On 우주로 ~")
+});
